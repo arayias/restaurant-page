6cc1af42f1c722a513ca9a4c21df401ed88a8a3d
Andreas Rayias <wowcaeu.andreas@gmail.com>
2023-08-16 01:29:28 +0300

    boilerplate for making the page with webpack and npm

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const mainContent = document.createElement("div");
+mainContent.attributes.id = "main-content";
+mainContent.innerHTML = `
+hello world! `;
+
+document.body.appendChild(mainContent);
